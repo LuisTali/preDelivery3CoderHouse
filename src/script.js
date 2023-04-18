87af8d3e8963de784a57f0208c8ff5f1faded29a
Luis Taliercio <taliercioluis1@gmail.com>
2023-04-17 21:39:02 -0300

    Add logic to set a User in the local storage when a person is registered, Add logic to validate the user in the login and recover his pets to be displayed

--- src/script.js
+++ src/script.js
@@ -2,28 +2,127 @@ import Gato from './Class/Gato.js'
 import Perro from './Class/Perro.js'
 import Oveja from './Class/Oveja.js'
 let mascotas = [];  
-const submitBtn = document.getElementById('buttonSubmit');
-const nameInput =document.getElementById('nameInput');
-const colorInput = document.getElementById('colorInput');
-const edadInput = document.getElementById('edadInput');
-const radioInputs = document.getElementsByName("tipoAnimal");
-const checkRadioBtn = document.getElementById("checkRadio");
-const inputExtraP = document.getElementById(`extraInputPerro`);
-const inputExtraG = document.getElementById(`extraInputGato`);    
-const inputExtraO = document.getElementById(`extraInputOveja`);
-const labelAnimalElegido = document.getElementById('animalElegido');
-let statusTipoElegido = 0;
 
+const mainContainer = document.getElementById("maincontainer");
+
+const submitBtn = document.getElementById('buttonSubmit'); //Boton para enviar los datos de mi mascota 
+const nameInput =document.getElementById('nameInput'); //Input para el nombre de mi mascota
+const colorInput = document.getElementById('colorInput'); //Input para el color de mi mascota
+const edadInput = document.getElementById('edadInput'); //Input para la edad de mi mascota
+const radioInputs = document.getElementsByName("tipoAnimal"); //Inputs para elegir tipo de mascota
+const checkRadioBtn = document.getElementById("checkRadio"); //Boton para chequear el tipo elegido
+const inputExtraP = document.getElementById(`extraInputPerro`); //Input extra si elegi Perro 
+const inputExtraG = document.getElementById(`extraInputGato`); //Input extra si elegi Gato   
+const inputExtraO = document.getElementById(`extraInputOveja`); //Input extra si elegi Oveja
+const labelAnimalElegido = document.getElementById('animalElegido'); //Label que indica el tipo elegido
+let statusTipoElegido = 0; //Flag que sirve para chequear si se definio el tipo de mascota
+
+const registerBtn = document.getElementById("registerBtn"); //Boton para mostrar el register-form
+const loginBtn = document.getElementById("loginBtn"); //Boton para mostrar el login-form
+
+const registerDiv = document.getElementById("register");
+const loginDiv = document.getElementById("login");
+
+const registerUserInput = document.getElementById("usernameInputRegister");
+const registerEmailInput = document.getElementById("emailInputRegister")
+const registerSubmitButton = document.getElementById("registerSubmitBtn");
+
+const loginUserInput = document.getElementById("usernameInputLogin");
+const loginEmailInput = document.getElementById("emailInputLogin")
+const loginSubmitButton = document.getElementById("loginSubmitBtn");
 
 const selectFiltrado = document.getElementById("filtrado"); //Para el filtrado por tipo de animal
-const quitarFiltroBtn = document.getElementById("quitarfiltro");
+const quitarFiltroBtn = document.getElementById("quitarfiltro"); //Boton para quitar el filtro por Tipo
 
-const buscarNombreBtn = document.getElementById("buscarNombre");
+const buscarNombreBtn = document.getElementById("buscarNombre"); //Boton para buscar por nombre
+
+let usuarioLogueado = null; //Al loguearse, se guarda aqui el username
 
 const inactiveExtraInputs = ()=>{
     (!inputExtraP.classList.contains('inactive') ? inputExtraP.classList.add('inactive') : null);
     (!inputExtraG.classList.contains('inactive') ? inputExtraG.classList.add('inactive') : null);
     (!inputExtraO.classList.contains('inactive') ? inputExtraO.classList.add('inactive') : null);
+}
+
+const crearPerro = (nombre,edad,color,tipoMascota,raza)=>{
+    let perro = new Perro(nombre,edad,color,tipoMascota,raza);
+    return perro;
+}
+const crearGato = (nombre,edad,color,tipoMascota,largoGarras)=>{
+    let gato = new Perro(nombre,edad,color,tipoMascota,largoGarras);
+    return gato;
+}
+const crearOveja = (nombre,edad,color,tipoMascota,despelada)=>{
+    let oveja = new Perro(nombre,edad,color,tipoMascota,despelada);
+    return oveja;
+}
+
+registerBtn.onclick = ()=>{
+    (loginDiv.classList.contains("inactive") ? "" : loginDiv.classList.add("inactive"))
+    registerDiv.classList.remove("inactive");
+    loginUserInput.value = "";
+    loginEmailInput.value = "";
+}
+
+registerSubmitButton.onclick = ()=>{
+    let username = registerUserInput.value;
+    let email = registerEmailInput.value;
+    if(username!="" && email!=""){
+        console.log(username, email);
+        localStorage.setItem("user",JSON.stringify({username:username,email:email}));
+        registerUserInput.value = "";
+        registerEmailInput.value = "";
+        mascotas = [];
+        localStorage.removeItem("mascotas");
+        mostrarAnimales()
+    }else{
+        alert("Llene todos los campos")
+    }
+}
+
+loginBtn.onclick = ()=>{
+    (registerDiv.classList.contains("inactive") ? "" : registerDiv.classList.add("inactive"))
+    loginDiv.classList.remove("inactive");
+    registerUserInput.value = "";
+    registerEmailInput.value = "";
+}
+
+loginSubmitButton.onclick = ()=>{
+    let username = loginUserInput.value;
+    let email = loginEmailInput.value;
+    if(username != "" && email != ""){
+        console.log(username, email);
+        let userValidation = JSON.parse(localStorage.getItem("user"));
+        if(username == userValidation.username && email == userValidation.email){
+            usuarioLogueado = username;
+            
+            mainContainer.classList.remove("inactive");
+            loginDiv.classList.toggle("inactive");
+
+            mascotas = []; //Para que si el usuario loguea dos veces, no se duplique el arreglo
+
+            document.getElementById("alert").innerHTML = `Bienvenido de nuevo ${username}!`
+            let mascotasParseadas = (JSON.parse(localStorage.getItem("mascotas"))).mascotas;
+            console.log(mascotasParseadas);
+            mascotasParseadas.forEach(mascota =>{
+                if(mascota.tipoMascota == "perro"){
+                    let perro = crearPerro(mascota.nombre,mascota.edad,mascota.color,mascota.tipoMascota,mascota.raza);
+                    mascotas.push(perro);
+                }else if(mascota.tipoMascota == "gato"){
+                    let gato = new Gato(mascota.nombre,mascota.edad,mascota.color,mascota.tipoMascota,mascota.largoGarras);
+                    mascotas.push(gato);
+                }else{
+                    let oveja = new Oveja(mascota.nombre,mascota.edad,mascota.color,mascota.tipoMascota,mascota.despelada);
+                    mascotas.push(oveja);
+                }
+            })
+            mostrarAnimales();
+        }else{
+            alert("Compruebe si los datos ingresados son correctos, tenga en cuenta mayusculas")
+        }
+    }else{
+        alert("Llene todos los campos")
+    }
 }
 
 const checkRadio = (e) =>{
@@ -117,6 +216,7 @@ const cargarMascota = (e) =>{
 
                 break;
         }
+        localStorage.setItem("mascotas",JSON.stringify({usuario:usuarioLogueado,mascotas:mascotas}));
         statusTipoElegido = 0;
         nameInput.value = "";
         edadInput.value = "";
@@ -162,11 +262,16 @@ const mostrarAnimales = (filtrado = "") =>{
             mascotasHtml.appendChild(parrafo);
         })
     }else{
-        mascotas.forEach(mascota => {
-            let parrafo = document.createElement('p');
-            parrafo.innerHTML = mascota.presentacion();
-            mascotasHtml.appendChild(parrafo);
-        });
+        if(mascotas.length == 0){
+            mascotasHtml.innerHTML = "";
+        }else{
+            mascotas.forEach(mascota => {
+                let parrafo = document.createElement('p');
+                parrafo.innerHTML = mascota.presentacion();
+                mascotasHtml.appendChild(parrafo);
+            });
+        }
+        
     }
 }
 
